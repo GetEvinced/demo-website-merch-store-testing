1b78b885c299b37ad8c6d10f91d05ffda4926707
Ben Bracha <ben.bracha@evinced.com>
2026-02-19 09:02:54 +0200

    demo-website first commit

---
+++ demo-website/src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import './HelloWorld.css';
+
+const HelloWorld = () => {
+  return (
+    <main className="hello-world">
+      <h1>Hello, World!</h1>
+      <p>Welcome to the Demo Website built with React and Webpack.</p>
+    </main>
+  );
+};
+
+export default HelloWorld;
